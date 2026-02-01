11741959be6cccde678fdb83942e8961d5a965bf
Hitalo da Tuf <hitalofrota@gmail.com>
2026-01-31 21:31:42 -0300

    feat: implementando nginx na aplicação

--- app/services/api.ts
+++ app/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Appointment } from "../types";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: "/api"
 });
 
 api.interceptors.request.use((config) => {
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,16 +1,23 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://scheduling_api:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
-    }
-  }
-});
+    },
+  },
+})
